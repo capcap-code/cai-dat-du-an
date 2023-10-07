2d33f270621e04c7470b284af9f2dcd7c7baa179
capcap-code <121926060+capcap-code@users.noreply.github.com>
2023-10-07 12:46:30 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const app = express();
 const contactsRouter = require("./app/routes/contact.route");
 
 
-app.use("/api/contacts", contactsRouter);
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) =>{
 	res.json({message:"Welcome to contact book application."});
 });
 
-app.use("/api/contacts", contactsRouter);
+// app.use("/api/contacts", contactsRouter);
 
 // handle 404 response
 app.use((req, res, next) => {
